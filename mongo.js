f0638e987e3863faea44dc4eb81c3c7336b95be2
javascriptForEverything <javascriptForEverything@gmail.com>
2021-12-25 21:15:41 +0600

    aggrigate has some cool features

--- mongo.js
+++ mongo.js
@@ -3,15 +3,40 @@ use test
 // show collections
 
 
-// db.persons.find().count() 		// => 1000
-// db.persons.find().pretty().limit(4) 		// => 1000
-
+// db.persons.find().pretty()
 // db.persons.aggregate().pretty()
-// db.persons.aggregate([{ $limit: 1 } ]).pretty() 						// => db.persons.find().pretty().limit(4)
-// db.persons.aggregate([{ $count: 'numVar' } ]).pretty() 		// => db.persons.find().count() 	=> 1000
+
+// db.persons.find().count()
+// db.persons.aggregate([{ $count: 'numVar' } ])
+
+// db.persons.find().limit(4).pretty()
+// db.persons.aggregate([{ $limit: 4 } ]).pretty()
+
+// db.persons.find({}, { _id:0, name:1,}).sort({ name: 1 }).pretty()
+// db.persons.aggregate([
+// 	{ $project: { _id:0, name:1 } },
+// 	{ $sort: { name: 1 } },
+// ]).pretty()
+
+
+// // Get unique value | count every fields duplicate value
+// db.persons.aggregate([{ $group: { _id: '$gender'}} ])
+// db.persons.aggregate([{ $group: { _id: { gender: '$gender'} }} ])
+// db.persons.aggregate([{ $group: { _id: '$gender', count: { $sum: 1 }}} ])
+
+// // Count Same Aged People total value
+// db.persons.aggregate([{ $group: { _id: '$age', age: { $sum: '$age' }}} ])
+
+
+// How to get unique value of and array field of every document ?
+// 		. { $unwind: '$array' } 				=> convert array => object with repeaded value but unique tags property
+// 		. { $group: { _id: '$tags'} } 	=> only get uniques value from every tags array of every document : new Set(...arr)
+// 		. Object.values(dbOutput) 			=> [...]
+
 
 // db.persons.aggregate([
-// 	{ $limit: 1 }
-// ]).pretty()
+// 	{ $unwind: '$tags' }, 					// convert array => object { ...arr, tags: [array[tags]][i] }
+// 	{ $group: { _id: '$tags'} } 		// only get uniques value from every tags array of every document : new Set(...arr)
+// ]).pretty() // count every fields duplicate value
 
 
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 ### MongoDB Aggrigate Pipeline / Framework
 
 Besic Steps:
+
 	1. start mongodb database
 
 		$ sudo systemctl start mongodb
@@ -27,3 +28,41 @@ Besic Steps:
 
 	 	db.persons.insertMany( <paste the array of JSON here> )
 
+
+	db.persons.find().pretty()
+	db.persons.aggregate().pretty()
+
+	db.persons.find().count()
+	db.persons.aggregate([{ $count: 'numVar' } ])
+
+	db.persons.find().limit(4).pretty()
+	db.persons.aggregate([{ $limit: 4 } ]).pretty()
+
+	db.persons.find({}, { _id:0, name:1,}).sort({ name: 1 }).pretty()
+
+	db.persons.aggregate([
+		{ $project: { _id:0, name:1 } },
+		{ $sort: { name: 1 } },
+	]).pretty()
+
+
+
+###### Get unique value | count every fields duplicate value
+	db.persons.aggregate([{ $group: { _id: '$gender'}} ])
+	db.persons.aggregate([{ $group: { _id: { gender: '$gender'} }} ])
+	db.persons.aggregate([{ $group: { _id: '$gender', count: { $sum: 1 }}} ])
+
+###### Count Same Aged People total value
+	db.persons.aggregate([{ $group: { _id: '$age', age: { $sum: '$age' }}} ])
+
+
+###### How to get unique value of and array field of every document ?
+	1. { $unwind: '$array' } 		=> convert array => object with repeaded value but unique tags property
+	2. { $group: { _id: '$tags'} } 	=> only get uniques value from every tags array of every document : new Set(...arr)
+	3. Object.values(dbOutput) 		=> [...]
+
+
+	db.persons.aggregate([
+		{ $unwind: '$tags' }, 		// convert array => object { ...arr, tags: [array[tags]][i] }
+		{ $group: { _id: '$tags'} } 	// only uniques value of every tags of every document : new Set(...arr)
+	]).pretty() // count every fields duplicate value
